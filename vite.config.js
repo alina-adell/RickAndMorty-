9740a58094ed11d4d54f5e87dcdc0d2d5e50b785
alina-adell <lyhaalina@gmail.com>
2025-03-17 23:42:00 +0100

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const repositoryName = 'https://alina-adell.github.io/RickAndMorty-/'; // Замените на имя вашего репозитория
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'RickAndMorty-',
 });
